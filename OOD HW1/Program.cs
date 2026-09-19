fb8c5c9771deedce72295ca62bdd8c9e2bd5c263
Millie Huss <max.huss@trojans.dsu.edu>
2026-09-19 06:02:46 -0500

    finished program.cs

--- OOD HW1/Program.cs
+++ OOD HW1/Program.cs
@@ -11,13 +11,13 @@ namespace OOD_HW1
             Square square1 = new Square();
             square1.Length = 12;
 
-            Console.WriteLine($"Length Square 1 of is {square1.Length}.");
+            Console.WriteLine($"Length of Square 1 of is {square1.Length}.");
             Console.WriteLine($"Perimeter is {square1.CalculatePerimeter()}");
             Console.WriteLine($"Area is {square1.CalculateArea()}");
 
             Square square2 = new Square(20);
 
-            Console.WriteLine($"Length Square 2 of is {square2.Length}.");
+            Console.WriteLine($"Length of Square 2 of is {square2.Length}.");
             Console.WriteLine($"Perimeter is {square2.CalculatePerimeter()}");
             Console.WriteLine($"Area is {square2.CalculateArea()}");
 
@@ -27,9 +27,17 @@ namespace OOD_HW1
             triangle1.Side1 = 8;
             triangle1.Side2 = 10;
 
+            Console.WriteLine($"Base of Triangle 1 is {triangle1.Base}. Height is {triangle1.Height}. Side 1 is {triangle1.Side1}. Side 2 is {triangle1.Side2}");
+            Console.WriteLine($"Perimeter is {triangle1.CalculatePerimeter()}");
+            Console.WriteLine($"Area is {triangle1.CalculateArea()}");
+
             Triangle triangle2 = new Triangle(12, 10, 15, 9);
 
-           
+            Console.WriteLine($"Base of Triangle 2 is {triangle2.Base}. Height is {triangle2.Height}. Side 1 is {triangle2.Side1}. Side 2 is {triangle2.Side2}");
+            Console.WriteLine($"Perimeter is {triangle2.CalculatePerimeter()}");
+            Console.WriteLine($"Area is {triangle2.CalculateArea()}");
+
+
         }
     }
 }
