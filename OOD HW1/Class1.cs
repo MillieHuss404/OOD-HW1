92b2786b337b75812976524c26aec1e6d5d1ed91
Millie Huss <max.huss@trojans.dsu.edu>
2026-09-19 04:46:13 -0500

    Added Square Class

--- OOD HW1/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace OOD_HW1
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ OOD HW1/Square.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOD_HW1
+{
+    internal class Square
+    {
+        //Attributes/Fields
+        public double Length;
+
+        //constructor
+        //Default Constructor
+        public Square()
+        {
+            Console.WriteLine("New Square instance was created");
+            Length = 0;
+
+        }
+
+        //Parameterized Constructor
+        public Square(double length)
+        {
+            Console.WriteLine("New Square instance was created");
+            Length = length;
+        }
+
+        //copy constructor
+        public Square(Square s)
+        {
+            Console.WriteLine("New Square instance was created");
+            Length = s.Length;
+        }
+
+        //behavior/methods
+        public double CalculateArea()
+        {
+            return Length*Length;
+        }
+
+        public double CalculatePerimeter()
+        {
+            return Length * 4;
+        }
+    }
+}
